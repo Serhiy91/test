9667d2a2cfd66bd197c37c6798f00c8e53ba0943
Serhiy91 <serhiyzi@mail.ru>
2015-10-08 13:57:08 +0300

    init gulp and bower

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var gulp = require("gulp"),
+	sass = require("gulp-sass"),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifyCSS = require("gulp-minify-css"),
+	rename = require("gulp-rename"),
+	concat = require("gulp-concat");
